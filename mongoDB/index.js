657309de339889866aa2cf37c1aa5b997192a11d
Claudisnei Alves <claudisneibello@gmail.com>
2022-01-25 21:49:39 -0300

    Realizando diversos tipos de busca no Article model

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -7,8 +7,20 @@ mongoose.connect("mongodb://localhost:27017/aprendendomongodb",{useNewUrlParser:
 //criando a tabela Article utilizando o esquema importado do article.
 const Article = mongoose.model('Article',articleModel);
 
-//criando o primeiro artigo
-const artigo = new Article({title: "Primeiro Artigo", author: "Eu mesmo", body:"Não sei mais..."});
+
+//criando os artigos
+
+const artigo = new Article({
+    title: "PHP", 
+    author: "Eu sei 3", 
+    body:"Não sei mais também...",
+    especial: false,
+    resume: {
+        content: "Resumo teste,....",
+        author: "Teste !"
+    }
+});
+
 
 //salvando o primeiro artigo no banco de dados.
 artigo.save().then(()=>{
@@ -16,4 +28,47 @@ artigo.save().then(()=>{
 }).catch(err =>{
     console.log(err);
 });
+
+
+
+/*
+
+//buscando todo os artigos
+Article.find({}).then(articles => {
+    console.log(articles);
+}).catch(err => {
+    console.log(err);
+})
+
+
+//buscando dados espedificos
+//buscando pelo id
+Article.find({'_id': '61f061a16af645ed3acc63bc'}).then(article => {
+    console.log(article);
+}).catch(err => {
+    console.log(err);
+})
+
+
+//buscando pelo title
+Article.find({'title': 'Primeiro Artigo'}).then(article => {
+    console.log(article);
+}).catch(err => {
+    console.log(err);
+})
+
+//buscando dados de campos que estão dentro de campos
+Article.find({'resume.author': 'Teste !'}).then(article => {
+    console.log(article);
+}).catch(err => {
+    console.log(err);
+})
+*/
+
+//mostrando o primeiro resultado de uma busca que corresponde a um critério
+Article.findOne({'author': 'Eu sei 3'}).then(article => {
+    console.log(article);
+}).catch(err => {
+    console.log(err);
+})
 
